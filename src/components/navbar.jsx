9893240258678d29ff7e3613228b46080ea70021
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-07-08 13:20:32 +0500

    responsive the website

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,24 +5,24 @@ const navbar = () => {
         <div className='border-2 border-gray-300 shadow-lg rounded-lg w-full mx-auto'>
             <header>
                 <nav className="bg-gray-800 p-4">
-                    <div className="flex justify-between items-center">
+                    <div className="flex lg:flex-row flex-col justify-between items-center">
                         <div className="text-white text-lg font-bold">
                             Techzoid
                         </div>
-                        <ul className="flex gap-4">
+                        <ul className="flex lg:flex-row flex-col gap-4">
                             <li><a href="#" className="text-white  hover:text-gray-300">Home</a></li>
                             <li><a href="#" className="text-white hover:text-gray-300">Our Game</a></li>
                             <li><a href="#" className="text-white hover:text-gray-300">Loding</a></li>
                             <li><a href="#" className="text-white hover:text-gray-300">Rate</a></li>
                             <li><a href="#" className="text-white hover:text-gray-300">Contact</a></li>
-                            <li><a href="#" className="text-white hover:text-gray-300   ">Book your Hunt</a></li>
+                            <li><a href="#" className="text-white hover:text-gray-300">Book your Hunt</a></li>
                         </ul>
-                        <ul className="flex justify-between border border-white rounded-full p-2 w-[150px] space-x-4 text-2xl">
+                        <ul className="flex justify-between border border-white rounded-full p-2 lg:w-[150px] w-8/12 space-x-4 text-2xl">
                             <li><a href="#" className="text-gray-300 hover:text-white"><i class="fab fa-facebook"></i></a></li>
                             <li><a href="#" className="text-gray-300 hover:text-white"><i class="fab fa-youtube"></i></a></li>
                             <li><a href="#" className="text-gray-300 hover:text-white"><i class="fas fa-envelope"></i></a></li>
                         </ul>
-                        <ul className="flex justify-center border border-white rounded-full p-2 w-2/12  space-x-4">
+                        <ul className="flex justify-center border border-white rounded-full p-2 lg:w-2/12 w-8/12  space-x-4">
                             <li><a href="#" className="text-gray-300 hover:text-white">Call Us (035463445645)</a></li>
                         </ul>
                     </div>
